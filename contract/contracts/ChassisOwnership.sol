a17bad8c7e7fedef5844f196f3ce3308805c791b
Leongard <vovchick2210@gmail.com>
2023-01-08 20:17:17 +0800

    Ownerships должны быть правильные наверное

--- contract/contracts/ChassisOwnership.sol
+++ contract/contracts/ChassisOwnership.sol
@@ -6,28 +6,27 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-contract CarOwnership is ERC721, Ownable{
-
+contract ChassisOwnership is ERC721, Ownable{
     TokenOwnership tokenOwnership;
 
-    constructor() ERC721("CarBucks", "CBX"){}
+    constructor() ERC721("ChassisBucks", "HBX"){}
 
     function init(address tokenOwnershipAddress) public onlyOwner{
         tokenOwnership = TokenOwnership(tokenOwnershipAddress);
     }
 
     function balanceOf(address _owner) override public view returns (uint256){
-        return tokenOwnership.balanceOfCars(_owner);
+        return tokenOwnership.balanceOfChassis(_owner);
     }
     function ownerOf(uint256 _tokenId) override public view returns (address){
-        return tokenOwnership.ownerOfCar(_tokenId);
+        return tokenOwnership.ownerOfChassis(_tokenId);
     }
     function transferFrom(address _from, address _to, uint256 _tokenId) override public {
-        tokenOwnership.transferCarFrom(_from, _to, _tokenId);
+        tokenOwnership.transferChassisFrom(_from, _to, _tokenId);
         emit Transfer(_from, _to, _tokenId);
     }
     function approve(address _approved, uint256 _tokenId) override public{
-        tokenOwnership.approveCar(_approved, _tokenId);
+        tokenOwnership.approveChassis(_approved, _tokenId);
         emit Approval(msg.sender, _approved, _tokenId);
     }
 
--- contract/contracts/EngineOwnership.sol
+++ contract/contracts/EngineOwnership.sol
@@ -6,28 +6,27 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-contract CarOwnership is ERC721, Ownable{
-
+contract EngineOwnership is ERC721, Ownable{
     TokenOwnership tokenOwnership;
 
-    constructor() ERC721("CarBucks", "CBX"){}
+    constructor() ERC721("EngineBucks", "EBX"){}
 
     function init(address tokenOwnershipAddress) public onlyOwner{
         tokenOwnership = TokenOwnership(tokenOwnershipAddress);
     }
 
     function balanceOf(address _owner) override public view returns (uint256){
-        return tokenOwnership.balanceOfCars(_owner);
+        return tokenOwnership.balanceOfEngines(_owner);
     }
     function ownerOf(uint256 _tokenId) override public view returns (address){
-        return tokenOwnership.ownerOfCar(_tokenId);
+        return tokenOwnership.ownerOfEngine(_tokenId);
     }
     function transferFrom(address _from, address _to, uint256 _tokenId) override public {
-        tokenOwnership.transferCarFrom(_from, _to, _tokenId);
+        tokenOwnership.transferEngineFrom(_from, _to, _tokenId);
         emit Transfer(_from, _to, _tokenId);
     }
     function approve(address _approved, uint256 _tokenId) override public{
-        tokenOwnership.approveCar(_approved, _tokenId);
+        tokenOwnership.approveEngine(_approved, _tokenId);
         emit Approval(msg.sender, _approved, _tokenId);
     }
 
