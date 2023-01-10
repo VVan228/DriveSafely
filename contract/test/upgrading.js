7aaf39df4d6d1f64c0b91532a2caddee0bfc8f01
rdashk <rdashak8@gmail.com>
2023-01-10 16:40:41 +0800

    upgradeCapacity

--- contract/test/upgrading.js
+++ contract/test/upgrading.js
@@ -1,18 +1,36 @@
 const CarHelper = artifacts.require("CarHelper");
 
+const capacityCoef = 0.00001;
+const productionPerHourCoef = 0.0002;
+const horsePowersCoef = 0.000015;
+const consumtionCoef = 0.005;
+const durabilityCoef = 0.0000000001;
+const levelUpCost = 0.001;
+
+const counts = 50;
+
 contract('CarUpgrade', (accounts) => {
 
     it('capacity is changed', async () => {
         const instance = await CarHelper.deployed();
-        const cars = await instance.onlyOwnerOfFuelStation(accounts[0]);
-        const newCapacity = cars[0].capacity + 50;
+        await instance.createCar("car1",{from:accounts[0]});
+        const cars = await instance.getCarsByOwner(accounts[0]);
+        const stations = await instance.getFuelStationByOwner(accounts[0]);
+        // checking balance
+        // not enough balance
+        const oldBalance = counts * stations[0].capacity * capacityCoef;
+        assert.equal(accounts[0].balance < oldBalance, true);
+
+        // upgrade capacity
+        const newCapacity = cars[0].capacity + counts;
         cars[0].upgradeCapacity(0, 50);
         assert.equal(cars[0].capacity, newCapacity);
     });
 
     it('production per hour is changed', async () => {
         const instance = await CarHelper.deployed();
-        const cars = await instance.onlyOwnerOfFuelStation(accounts[0]);
+        await instance.createCar("car1",{from:accounts[0]});
+        const cars = await instance.getCarsByOwner(accounts[0]);
         const newProductionPerHour = cars[0].productionPerHour + 50;
         cars[0].upgradeProductionPerHour(0, 50);
         assert.equal(cars[0].productionPerHour, newProductionPerHour);
@@ -20,7 +38,8 @@ contract('CarUpgrade', (accounts) => {
 
     it('horse powers is changed', async () => {
         const instance = await CarHelper.deployed();
-        const cars = await instance.onlyOwnerOfEngine(accounts[0]);
+        await instance.createCar("car1",{from:accounts[0]});
+        const cars = await instance.getCarsByOwner(accounts[0]);
         const newHorsePowers = cars[0].horsePowers + 50;
         cars[0].upgradeHorsePowers(0, 50);
         assert.equal(cars[0].horsePowers, newHorsePowers);
@@ -28,7 +47,8 @@ contract('CarUpgrade', (accounts) => {
 
     it('consumtion is changed', async () => {
         const instance = await CarHelper.deployed();
-        const cars = await instance.onlyOwnerOfEngine(accounts[0]);
+        await instance.createCar("car1",{from:accounts[0]});
+        const cars = await instance.getCarsByOwner(accounts[0]);
         const newConsumtion = cars[0].consumtion + 50;
         cars[0].upgradeConsumtion(0, 50);
         assert.equal(cars[0].consumtion, newConsumtion);
@@ -36,7 +56,8 @@ contract('CarUpgrade', (accounts) => {
 
     it('durability is changed', async () => {
         const instance = await CarHelper.deployed();
-        const cars = await instance.onlyOwnerOfChassis(accounts[0]);
+        await instance.createCar("car1",{from:accounts[0]});
+        const cars = await instance.getCarsByOwner(accounts[0]);
         const newDurability = cars[0].durability + 50;
         cars[0].upgradeDurability(0, 50);
         assert.equal(cars[0].durability, newDurability);
@@ -44,11 +65,11 @@ contract('CarUpgrade', (accounts) => {
 
     it('level increased by 1', async () => {
         const instance = await CarHelper.deployed();
+        await instance.createCar("car1",{from:accounts[0]});
         const cars = await instance.getCarsByOwner(accounts[0]);
-        const newLevel = cars[0].carLevel+1;
         levelUp(0);
         // level increased by 1
-        assert.equal(cars[0].carLevel, newLevel);
+        assert.equal(cars[0].carLevel, 1);
         // reset win counter
         assert.equal(cars[0].winCountOnCurrentLevel, 0);
         // reset defeat counter
