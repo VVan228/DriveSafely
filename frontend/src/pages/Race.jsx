f7f4112f8aec3e4c34ab2699d4ada3b9b64d306e
Ilya Plakhotin <72680908+loud1y@users.noreply.github.com>
2023-01-11 14:51:17 +0800

    ready for cars positioning

--- frontend/src/pages/Race.jsx
+++ frontend/src/pages/Race.jsx
@@ -6,6 +6,8 @@ import {useFetching} from "../hooks/useFetching";
 import Loader from "../components/UI/loader/Loader";
 import {useParams} from "react-router-dom";
 import {getDummyCars} from "../utils/cars";
+import Car from "../components/Car";
+import {getCarPosition} from "../utils/race";
 
 const Race = () => {
 
@@ -16,7 +18,16 @@ const Race = () => {
 
     const roadType = Math.floor(Math.random() * 2)
     const testCars = getDummyCars(Math.floor(Math.random() * ((roadType === 0 ? 5 : 4) - 2) + 2))
-
+    const testCarsComponents = testCars.map((car, index)=>
+        <Car
+            key={car.id}
+            car={car}
+            position={getCarPosition(index, windowWidth / windowHeight)}
+            width="5%"
+            rotate={90 * index}
+        />
+    )
+    console.log(testCarsComponents)
 
 
     const [waitForPlayers, isRoomLoading, error] = useFetching(async () => {
@@ -44,12 +55,15 @@ const Race = () => {
             <div className="h-100 w-100 d-flex align-items-center justify-content-center"><Loader/></div> :
             <div className={classes.raceBackground}>
                 <div style={{width: "100vw"}}>
+                    <div className="position-absolute top-0 text-dark bg-light">
+                        <h1 className="">Машин: {testCars.length}</h1>
+                        {testCarsComponents.map(car =>
+                            <h3 className="">{car.props.rotate / 90}: ({car.props.position.x}%,{car.props.position.y}%)</h3>
+                        )}
+                    </div>
+
                     <img src={roadType == 0 ? background : background2 } alt="" width="100%" style={{}}/>
-                    <div className={classes.testDot} style={{
-                        transition: ".3s",
-                        top: `${windowWidth / windowHeight / (windowWidth / windowHeight) * 70 }%`,
-                        left: '54%',
-                    }}></div>
+                    {testCarsComponents.map(testCar => testCar)}
                 </div>
             </div>
     );
