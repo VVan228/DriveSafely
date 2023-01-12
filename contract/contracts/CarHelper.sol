bef00621d073683c54f9f3282b3825046c03a72f
rdashk <rdashak8@gmail.com>
2023-01-12 15:07:53 +0800

    add comments to carHelper

--- contract/contracts/CarHelper.sol
+++ contract/contracts/CarHelper.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "./CarFactory.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 
-
+/// @title cars
 contract CarHelper is CarFactory {
 
     uint capacityCoef = 0.00001 ether;
@@ -14,26 +14,33 @@ contract CarHelper is CarFactory {
     uint durabilityCoef = 0.0000000001 ether;
     uint levelUpCost = 0.001 ether;
 
+    /// caller is equal car owner
     modifier onlyOwnerOfCar(uint _carId) {
         require(msg.sender == carToOwner[_carId]);
         _;
     }
 
+    /// caller is equal engine owner
     modifier onlyOwnerOfEngine(uint _engineId) {
         require(msg.sender == engineToOwner[_engineId]);
         _;
     }
 
+    /// caller is equal chassis owner
     modifier onlyOwnerOfChassis(uint _chassisId) {
         require(msg.sender == chassisToOwner[_chassisId], "chassis not your >:(");
         _;
     }
 
+    /// caller is equal fuel station owner
     modifier onlyOwnerOfFuelStation(uint _fuelStationId) {
         require(msg.sender == fuelStationToOwner[_fuelStationId]);
         _;
     }
 
+    /// changing engine in owner's car
+    /// @param carId car id
+    /// @param engineId engine id
     function switchEngine(uint carId, uint engineId) public onlyOwnerOfEngine(engineId) onlyOwnerOfCar(carId){
         require(cars[carId].engineId != engineId, "same engine");
         Car[] memory cc = getCarsByOwner(msg.sender);
@@ -45,6 +52,9 @@ contract CarHelper is CarFactory {
         cars[carId].engineId = engineId;
     }
 
+    /// changing chassis in owner's car
+    /// @param carId car id
+    /// @param chassisId chassis id
     function switchChassis(uint carId, uint chassisId) public onlyOwnerOfChassis(chassisId) onlyOwnerOfCar(carId){
         require(cars[carId].chassisId != chassisId, "same chassis");
         Car[] memory cc = getCarsByOwner(msg.sender);
@@ -56,16 +66,28 @@ contract CarHelper is CarFactory {
         cars[carId].chassisId = chassisId;
     }
 
+    /// removal of the old engine
+    /// @param carId car id
+    /// @param engineId engine id
+    /// @dev for installation new engine
     function detachEngine(uint carId, uint engineId) public onlyOwnerOfEngine(engineId) onlyOwnerOfCar(carId){
         require(cars[carId].engineId == engineId, "engine is not attached");
         cars[carId].engineId = 0;
     }
+
+    /// removal of the old chassis
+    /// @param carId car id
+    /// @param chassisId chassis id
+    /// @dev for installation new chassis
     function detachChassis(uint carId, uint chassisId) public onlyOwnerOfChassis(chassisId) onlyOwnerOfCar(carId){
         require(cars[carId].chassisId == chassisId, "chassis are not attached");
         cars[carId].chassisId = 0;
     }
 
-
+    /// fuel station of owner
+    /// @param _owner owner's address
+    /// @return FuelStation memory one owner fuel station
+    /// @dev exist only one
     function getFuelStationByOwner(address _owner) public view returns(FuelStation memory){
         uint resId;
         for (uint i = 0; i < stations.length; i++) {
@@ -77,6 +99,9 @@ contract CarHelper is CarFactory {
         return stations[resId];
     }
 
+    /// list of owner engines
+    /// @param _owner owner's address
+    /// @return Engine[] memory owner's list of engines
     function getEnginesByOwner(address _owner) public view returns(Engine[] memory) {
         Engine[] memory result = new Engine[](ownerEngineCount[_owner]);
         uint counter = 0;
@@ -89,6 +114,9 @@ contract CarHelper is CarFactory {
         return result;
     }
 
+    /// list of owner chassis
+    /// @param _owner owner's address
+    /// @return Chassis[] memory owner's list of engines
     function getChassisByOwner(address _owner) public view returns(Chassis[] memory) {
         Chassis[] memory result = new Chassis[](ownerChassisCount[_owner]);
         uint counter = 0;
@@ -101,6 +129,9 @@ contract CarHelper is CarFactory {
         return result;
     }
 
+    /// list of owner cars
+    /// @param _owner owner's address
+    /// @return Car[] memory owner's list of cars
     function getCarsByOwner(address _owner) public view returns(Car[] memory) {
         Car[] memory result = new Car[](ownerCarCount[_owner]);
         uint counter = 0;
@@ -113,36 +144,53 @@ contract CarHelper is CarFactory {
         return result;
     }
 
+    /// upgrade capacity of fuel station
+    /// @param _fuelStatonId fuel station id
+    /// @param _counts size
     function upgradeCapacity(uint _fuelStatonId, uint8 _counts) external payable onlyOwnerOfFuelStation(_fuelStatonId) {
         require(msg.value == (_counts * stations[_fuelStatonId].capacity * capacityCoef));
         payable(owner()).transfer(msg.value);
         stations[_fuelStatonId].capacity += _counts; 
     }
 
+    /// upgrade production on per hour
+    /// @param _fuelStatonId fuel station id
+    /// @param _counts size
     function upgradeProductionPerHour(uint _fuelStatonId, uint8 _counts) external payable onlyOwnerOfFuelStation(_fuelStatonId) {
         require(msg.value == (_counts * stations[_fuelStatonId].productionPerHour * productionPerHourCoef));
         payable(owner()).transfer(msg.value);
         stations[_fuelStatonId].productionPerHour += _counts; 
     }
 
+    /// upgrade horse powers
+    /// @param _engineId engine id
+    /// @param _counts size
     function upgradeHorsePowers(uint _engineId, uint8 _counts) external payable onlyOwnerOfEngine(_engineId) {
         require(msg.value == (_counts * engines[_engineId].horsePowers * horsePowersCoef));
         payable(owner()).transfer(msg.value);
         engines[_engineId].horsePowers += _counts; 
     }
 
+    /// upgrade consumption
+    /// @param _engineId engine id
+    /// @param _counts size
     function upgradeConsumtion(uint _engineId, uint8 _counts) external payable onlyOwnerOfEngine(_engineId) {
         require(msg.value == consumtionCoef * 2**(10 - engines[_engineId].consumtion));
         payable(owner()).transfer(msg.value);
         engines[_engineId].consumtion -= _counts; 
     }
 
+    /// upgrade durability
+    /// @param _chassisId chassis id
+    /// @param _counts size
     function upgradeDurability(uint _chassisId, uint8 _counts) external payable onlyOwnerOfChassis(_chassisId) {
         require(msg.value == (_counts * chassis[_chassisId].durability * durabilityCoef));
         payable(owner()).transfer(msg.value);
         chassis[_chassisId].durability += _counts; 
     }
 
+    /// upgrade car level
+    /// @param _carId car id
     function levelUp(uint _carId) external payable {
         uint r = levelUpCost * (cars[_carId].carLevel/10 + 1);
         require(msg.value == r, "wrong cost");
